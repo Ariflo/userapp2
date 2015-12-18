65cf8e697e6cc81c31fc8e79249eacac22bc8c42
AriFlo <ariflo@arianflores.com>
2015-12-18 15:44:23 -0800

    finished creating userapp2

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var bodyParser = require("body-parser");
 var knex = require ("./db/knex");
 var morgan = require("morgan");
 var locus = require("locus");
+var methodOverride = require("method-override");
 
 //middleware 
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan('tiny'));
+app.use(methodOverride('_method'));
 
 app.get('/students', function(req,res){
 	knex('students').then(function(students){
@@ -24,6 +26,27 @@ app.get('/students/new', function(req,res){
 app.post('/students', function(req, res){	
 	knex('students').insert(req.body).then(function(){
 		res.redirect('/students'); 	
+	});
+});
+
+app.get('/students/:id/edit', function(req, res){
+	//find a student 
+	var id = req.params.id 
+	knex('students').where({id:id}).first().then(function(student){
+		res.render('edit', {student:student}); 
+	});
+});
+
+app.put('/students/:id', function(req, res){	
+	knex('students').where({id:req.params.id}).first().update(req.body).then(function(){
+		res.redirect('/students');
+	});
+	
+});
+
+app.delete('/students/:id', function(req, res){
+	knex('students').where({id:req.params.id}).del().then(function(){
+		res.redirect('/students');
 	});
 });
 
